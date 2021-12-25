9cb2582166dfb9ea5375157457d4fc8869ac80e7
gost <kishor81160@gmail.com>
2021-12-25 19:02:54 +0530

    Necessary Library Has been Installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FontawesomeObject} from '@fortawesome/fontawesome-svg-core';
+import {} from '@fortawesome/free-solid-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
